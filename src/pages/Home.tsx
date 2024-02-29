3b8a30847f04045666b60abd90e3397538c29ab2
Sophie Y <javachiphi@gmail.com>
2024-02-29 15:09:36 +0800

    add 3d island object

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,23 @@ import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Island from '../models/Island';
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, rotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -16,7 +33,11 @@ const Home = () => {
           <directionalLight />
           <ambientLight />
           <pointLight />
-          <Island />
+          <Island
+            scale={islandScale}
+            position={islandPosition}
+            rotation={rotation}
+          />
           <hemisphereLight />
         </Suspense>
       </Canvas>
