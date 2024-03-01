67143ef8659aa96d72fe16005fdf7088d854c241
Sophie Y <javachiphi@gmail.com>
2024-03-01 15:40:22 +0800

    fix plane position

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -69,8 +69,8 @@ const Home = () => {
             setCurrentStage={setCurrentStage}
           />
           <Plane
-            planeScale={planeScale}
-            planePosition={planePosition}
+            scale={planeScale}
+            position={planePosition}
             isRotating={isRotating}
             rotation={[0, 20, 0]}
             currentStage={currentStage}
